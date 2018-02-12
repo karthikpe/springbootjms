8c03a0fa0ebc5971b8b97fb07229fab4954a53e4
Karthik Ekambaram <k.ekambaram@replyltd.co.uk>
2018-02-12 18:42:47 +0000

    Logger added.

--- src/main/java/com/poc/controller/BookingUpdatedEventController.java
+++ src/main/java/com/poc/controller/BookingUpdatedEventController.java
@@ -1,5 +1,7 @@
 package com.poc.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.poc.event.airport.booking.update.dto.BookingUpdatedAtAirportDTO;
 import com.poc.processor.AbstractEventProcessor;
+import com.poc.utils.JsonUtils;
 
 @RestController
 public class BookingUpdatedEventController {
 
+	private static final Logger LOG = Logger.getLogger(BookingUpdatedEventController.class.getName());
+	
 	@Autowired
 	AbstractEventProcessor<BookingUpdatedAtAirportDTO> processor;
 
 	@RequestMapping(value = "/event", method = RequestMethod.POST)
 	void processEvent(@RequestBody final BookingUpdatedAtAirportDTO eventDto) {
+		LOG.info("Booking updated event data:" + JsonUtils.getJson(eventDto));
 		processor.processEventMessage(eventDto);
 	}
 
